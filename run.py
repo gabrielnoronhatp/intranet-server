b6a85be8924dc81d3baf6cefa9d446973290a768
Gabriel Ximenes Mitozo Noronha <gabriel.noronha@grupotapajos.com.br>
2025-02-25 17:50:50 -0400

    add cors config

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = create_app()
 
 if __name__ == '__main__':
   
-    PORT = config('PORT', default=5000, cast=int)
+    PORT = config('PORT', default=3002, cast=int)
     DEBUG = config('DEBUG', default=True, cast=bool)
     HOST = config('HOST', default='0.0.0.0')
     app.run(
